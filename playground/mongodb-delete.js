179d520d9c437e510a2f594d8b8b9afd50f7f6e3
Terrance <Terrance.uy@pathmazing.com>
2018-02-26 09:37:06 +0700

    comit deletet

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,26 @@
+const {MongoClient, ObjectID} = require('mongodb');
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('connected to MongoDB server');
+
+  // Delete many
+  // db.collection('Todos').deleteMany({text: 'exercise'}).then((result)=>{
+  //   console.log("Deleted success", result)
+  // }, (error) => {
+  //   console.log("unable to deleteMany")
+  // })
+  // DeleteOne
+  // db.collection('Todos').deleteOne({text: 'exercise'}).then((result)=>{
+  //   console.log("Deleted success", result)
+  // }, (error) => {
+  //   console.log("unable to deleteMany")
+  // })
+  // findOneAndDelete
+  db.collection('Todos').findOneAndDelete({text: 'exercise'}).then((result)=>{
+    console.log("Deleted success", result)
+  }, (error) => {
+    console.log("unable to deleteMany")
+  })
+});

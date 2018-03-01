c0050296c8a9b9ebbc31901903250c944b0de7c0
Terrance <Terrance.uy@pathmazing.com>
2018-03-01 16:10:35 +0700

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,10 +3,9 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongodb://terrance:terrance@ds151528.mlab.com:51528/tododb
 console.log("process.env", process.env.MONGODB_URI)
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect("mongodb://terrance:terrance@ds151528.mlab.com:51528/tododb", {
   useMongoClient: true
-  }
-);
+});
 
 module.exports = {
   mongoose

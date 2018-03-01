830aa773342a26c24056e4518baf57b5ba0fe3f4
Terrance <Terrance.uy@pathmazing.com>
2018-03-01 14:36:39 +0700

    change to mongodblab_uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // mongodb://terrance:terrance@ds151528.mlab.com:51528/tododb
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODBLAB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
   mongoose
